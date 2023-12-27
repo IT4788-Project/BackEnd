04d677e2efd9e4d1fa61ec827ff93f5c2b4d675f
Hoàng Trung <trung@MacBook-Pros-Trung.local>
2023-12-27 23:51:05 +0700

    update

--- src/controllers/healthyGoalController.js
+++ src/controllers/healthyGoalController.js
@@ -102,7 +102,7 @@ const getOneHealthyGoal = async (req, res) => {
         userId: userId,
       }
     });
-    if (healthyGoal === 0) {
+    if (!healthyGoal) {
       return res.status(404).json({
         statusCode: 404,
         message: "Not Found",
